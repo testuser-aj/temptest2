b833fcc2ae304a6a64b3e9de299fa7e6b64b5e13
Ajay Kannan <ajaykannan@google.com>
2015-09-02 11:26:41 -0700

    bugz

--- utilities/update_version.sh
+++ utilities/update_version.sh
@@ -49,5 +49,7 @@ git add gcloud-java-storage/pom.xml
 git config --global user.name "travis-ci"
 git config --global user.email "travis@travis-ci.org"
 git commit -m "Updating version in README and pom.xml files."
-#git config --global push.default simple
-git push --quiet "https://${CI_DEPLOY_USERNAME}:${CI_DEPLOY_PASSWORD}@github.com/testuser-aj/temptest2.git" HEAD:origin/master # /dev/null 2>&1
+git config --global push.default simple
+echo | git remote -v
+git remote set origin https://github.com/testuser-aj/temptest2/
+git push --quiet "https://${CI_DEPLOY_USERNAME}:${CI_DEPLOY_PASSWORD}@github.com/testuser-aj/temptest2.git" HEAD:origin/master #> /dev/null 2>&1
